91c75f38fa7f238666e551db6bf071a91f2e25ff
Chad Coates <chroco@gmail.com>
2023-05-27 20:04:15 -0700

    Fucked the arrays

--- twosum.c
+++ twosum.c
@@ -6,15 +6,20 @@ int main(int argc, char *argv[])
   (void)argc;
   (void)argv;
 
-  //printf("%"PRId64"\n",MIN_NUMBER);
-  printf("%"PRId64"\n",MAX_NUMBER);
+  int seed = 31;
+  srand(seed);
+
+  printf("%"PRId64"\n",(int64_t)MIN_NUMBER);
+  printf("%"PRId64"\n",(int64_t)MAX_NUMBER);
     
-  const int arraySize = 4;
-  int64_t numArray[] = {2, 7, 11, 15};
-  //int64_t numArray[arraySize];
+  const int arraySize = 100;
+  int64_t numArray[arraySize];
   int64_t target = 9;
 
-  sumTwoOrderN2(target,numArray,arraySize);
+  memset(numArray, 0, 100 * sizeof(int64_t));
+
+  //initArray(numArray, arraySize);
+  printArray(numArray, arraySize); 
   //sumTwoOrderN2(target,numArray,arraySize);
 
   return 0;
--- twosum.h
+++ twosum.h
@@ -1,21 +1,63 @@
 #ifndef TWOSUM_H
 #define TWOSUM_H
 
+#include "time.h"
+#include "stdlib.h"
 #include "stdio.h"
 #include "stdint.h"
 #include "inttypes.h"
+#include "string.h"
 
-#define MIN_ARRAY_SIZE 2
-#define MAX_ARRAY_SIZE 10e4
-#define MIN_NUMBER     -10e9
-#define MAX_NUMBER     10e9
+#define MIN_ARRAY_SIZE  2
+#define MAX_ARRAY_SIZE  10e4
+#define MIN_NUMBER      -10e9
+#define MAX_NUMBER      10e9
+//#define RAND_MODIFIER   2 * MAX_NUMBER + 1  
 
-int fillArray(int64_t nArray[], const int size)
+int initArray(int64_t *pArray, const int size)
 {
-
+  int i;
+  const int64_t upperBound = (int64_t)MAX_NUMBER;
+  const int64_t spread = 2 * upperBound;
+  int64_t tmp;
+  
+  printf("%"PRId64"\n", INT64_MAX);
+  printf("%"PRId64"\n", upperBound);
+  printf("%"PRId64"\n", spread);
+  
+  for(i = 0; i < size; ++i)
+  {
+    //tmp = rand() % (spread + 1);
+    //tmp = tmp - upperBound; 
+   // *(pArray + i) = tmp;
+    //*(pArray + i) = rand() % (spread + 1) - upperBound;
+    *(pArray + i) = i;
+    //*(pArray + i) = ((int64_t)(rand() % (spread + 1)) - upperBound);
+  }
+  
   return 0;
 }
 
+int printArray(int64_t *pArray, const int size)
+{
+  int i;
+  // printf("target: %"PRId64 "\n", tar);
+  printf("array:");
+  
+  for(i = 0; i < size; ++i)
+  {
+    if(i % 10 == 0)
+    {
+      printf("\n");
+    }
+
+    printf("%"PRId64" ", *(pArray + i));
+  }
+  
+  printf("\n");
+
+  return 0;
+}
 
 int sumTwoOrderN2(int64_t tar, int64_t nArray[], const int size)
 {
@@ -23,16 +65,6 @@ int sumTwoOrderN2(int64_t tar, int64_t nArray[], const int size)
   int i; 
   int j;
   
-  printf("target: %"PRId64 "\n", tar);
-  printf("array: ");
-  
-  for(i = 0; i < size; ++i)
-  {
-    printf("%"PRId64" ", nArray[i]);
-  }
-  
-  printf("\n");
-
   for(i = 0; i < size; ++i)
   {
     for(j = 0; j < size; ++j)
